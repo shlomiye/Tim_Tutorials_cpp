ba7cb37ed75b32b1a881f0a4f65787ad30b638d7
Shlomi86 <shlomiy86@gmail.com>
2021-04-11 19:08:12 +0300

    Tutorial 4

---
+++ tutorial_4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int x = 8;
+	int y = 9;
+    int sum = x + y;
+    cout << sum << endl;
+    cout << x / y << endl;
+    float  a = 8.0;
+    float b = 9.0;
+    float div = a / b;
+    cout << div << endl; 
+
+    int t = 8;
+    int s = 5;
+    cout << t % s; 
+
+}
